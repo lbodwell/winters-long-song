942b6130cfe780cee4e81ee64bd2eafe46137f27
CJACOBSON32 <34342644+CJACOBSON32@users.noreply.github.com>
2018-09-16 18:02:28 -0400

    Switch working

---
+++ Winter's Long Song/Assets/Scripts/Wall_Behavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall_Behavior : MonoBehaviour {
+
+    public Vector3 offset = new Vector3(0, 2, 0);
+    public float speed = 1;
+
+    bool activated = false;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        endPos = transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    IEnumerator move()
+    {
+        float startTime = Time.time;
+        Vector3 start = transform.position;
+        Vector3 target;
+
+        if (activated) { target = startPos; }
+        else { target = endPos; }
+
+        //Debug.Log("target: " + target);
+
+        activated = !activated;
+
+        float totalDistance = Vector3.Distance(start, target);
+
+        while (transform.position != target)
+        {
+            float currentDuration = (Time.time - startTime) * speed;
+            float journeyFraction = currentDuration / totalDistance;
+
+            transform.position = Vector3.Lerp(start, target, journeyFraction);
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+
+        //Debug.Log("Reached destination");
+
+        yield return null;
+    }
+}
