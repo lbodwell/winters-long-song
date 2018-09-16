876003c8b9bc381ced45467fe728bebf02da2a22
CJACOBSON32 <34342644+CJACOBSON32@users.noreply.github.com>
2018-09-16 15:47:31 -0400

    Better Organized Tiles and Started Switchbehavior

---
+++ Winter's Long Song/Assets/Scripts/Switch_Behavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+public class Switch_Behavior : MonoBehaviour {
+
+    public enum actions { openclose }
+
+    public bool state = false;
+    public GameObject target;
+    public actions action = actions.openclose;
+
+
+    Collider2D trigger;
+
+    SpriteRenderer spriteR;
+    Sprite onSprite;
+    Sprite offSprite;
+
+    // Use this for initialization
+    void Start () {
+        trigger = GetComponent<Collider2D>();
+
+        spriteR = GetComponent<SpriteRenderer>();
+
+        onSprite = Resources.Load<Sprite>("Assets/Art/Sprites/Objects/Resources/oza_SwitchUp01_v01.png");
+        offSprite = Resources.Load<Sprite>("Assets/Art/Sprites/Objects/Resources/oza_SwitchDown01_v01.png");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        changeSprite(state);
+
+
+	}
+
+    void changeSprite(bool s)
+    {
+        if(s)
+        {
+            spriteR.sprite = onSprite;
+        }
+        else { spriteR.sprite = offSprite; }
+    }
+}
